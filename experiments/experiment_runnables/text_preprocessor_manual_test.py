f15ac1e6ab452472b94c1edf78d36f4f67a33156
Frank Jia <jiafrank98@gmail.com>
2020-02-29 09:33:02 -0800

    improve number processing

--- experiments/experiment_runnables/text_preprocessor_manual_test.py
+++ experiments/experiment_runnables/text_preprocessor_manual_test.py
@@ -2,7 +2,13 @@ from experiments.util.experiment_util import get_text_preprocessor
 
 
 def main():
-    text = "Mark Harris Says Democratic North Carolina House candidate Dan McCready \"took money from the Pelosi crowd\" and is \"with them\" in their agenda and \"wants to repeal your tax cut.\""
+    text = "McConnell's promise to keep the ability to filibuster legislation could make it more difficult for " \
+           "Republicans to get key parts of Trump's legislative agenda through the Senate, considering the expected " \
+           "strong Democratic opposition. 'Break the rules' A filibuster requires a super-majority of \"60 votes\" in the .5 or -1 " \
+           "100-seat Senate in order to proceed to a simple majority vote on a Supreme Court nominee or legislation for 600,000 participants of 200,000. " \
+           "The 60-vote super-majority threshold that gives the minority party power to hold up the majority party " \
+           "has forced the Senate over the decades to try to achieve bipartisanship in legislation and presidential " \
+           "appointments. "
     preprocessor = get_text_preprocessor()
     sentences = preprocessor.process(text).bert_sentences
     print("\n".join(sentences))
